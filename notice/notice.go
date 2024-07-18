629a604bacbd229d90a3800b5a821413866ae96b
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-07-18 16:19:20 +1000

    Updated notice logic in notice.go.

--- notice/notice.go
+++ notice/notice.go
@@ -1,4 +1,10 @@
 package notice
 
+import "fmt"
+
+func PrintLicenseNotice() {
+  fmt.Println("This program is licensed under the MIT license")
+  fmt.Println("Run: 'idb license' for a copy of the MIT license")
+}
 
 
