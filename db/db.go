4a15fac8f42348fdf993b68ce9703c4b83a96393
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-07-18 17:28:06 +1000

    Updated database schema in db.go.

--- db/db.go
+++ db/db.go
@@ -1,12 +1,17 @@
 package db
 
 import (
-  "github.com/jmoiron/sqlx"
-  _ "github.com/mattn/go-sqlite3"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func ConnectToDB() (*sqlx.DB, error) {
-  return sqlx.Open("sqlite3", "./idb.sqlite3")
+  db, err := sqlx.Open("sqlite3", "./idb.sqlite3")
+  if err != nil {
+    return nil, err
+  }
+
+  return db, db.Ping()
 }
 
 func CloseDB(db *sqlx.DB) {
