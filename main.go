d56eff9b1b3f8b4187a5e8427cedad0372226f16
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-07-18 16:23:00 +1000

    Updated main.go with new modifications.

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/aixoio/idb/notice"
+import (
+	_ "embed"
+	"os"
+	"strings"
+
+	"github.com/aixoio/idb/notice"
+)
+
+//go:embed LICENSE
+var license string
 
 func main() {
   notice.PrintLicenseNotice()
+  
+  if strings.Compare(strings.ToLower(os.Args[1]), "license") == 0 {
+    notice.PrintMITLicense(license)
+  }
 }
 
