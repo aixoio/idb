94aa47e660c01041bd93f6eb5896f7c9de44b787
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-07-18 17:19:57 +1000

    Update db.go and go.mod, add go.sum to tracked files.

--- db/db.go
+++ db/db.go
@@ -1,4 +1,11 @@
 package db
 
+import (
+  "github.com/jmoiron/sqlx"
+  _ "github.com/mattn/go-sqlite3"
+)
 
+func ConnectToDB() (*sqlx.DB, error) {
+  return sqlx.Open("sqlite3", "./idb.sqlite3")
+}
 
