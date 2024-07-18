f344d79081a9acf112fcea2307b9f2f9a92ee0d7
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-07-18 20:23:46 +1000

    Updated schema definition in db/schema.go.

--- db/schema.go
+++ db/schema.go
@@ -2,12 +2,20 @@ package db
 
 import (
 	_ "embed"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 )
 
 //go:embed schema.sql
 var schema string
+
+type Idea struct {
+  Id int
+  Idea string
+  Created_At time.Time
+  Updated_At time.Time
+}
 
 func CreateDatabaseSchemaIfNotExists(db *sqlx.DB) error { 
   _, err := db.Exec(schema)
